9d7855aa8062b785be9806852d6976f2486cd479
polodev <polodev10@gmail.com>
2019-10-05 00:47:01 +0600

    install laravel

---
+++ app/Tag.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+  protected $guarded = [];
+  public function posts()
+  {
+    return $this->belongsToMany( Post::class );
+  }
+  public function logs()
+  {
+    return $this->belongsToMany( Post::class )->where( 'type', 'log' );
+  }
+  public function gists()
+  {
+    return $this->belongsToMany( Post::class )->where( 'type', 'gist' );
+  }
+  public function tuts()
+  {
+    return $this->belongsToMany( Post::class )->where( 'type', 'tut' );
+  }
+}
